a51d0309f2362dbc68f41d0eda0d63383e135261
Treasure Ajefu <treasureajefu@gmail.com>
2022-10-03 22:42:19 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-  return <h1 className="text-3xl font-bold text-red-600">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold text-red-600">I4G Hackfest project assigned to cybergeni!</h1>;
 }
 
 export default App;
