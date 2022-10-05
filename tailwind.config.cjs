b3b5d1ae9f2f693f0f5d209a7218db65221a45cc
Treasure Ajefu <treasureajefu@gmail.com>
2022-10-05 11:04:06 +0100

    added font to tailwind config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -76,6 +77,9 @@ module.exports = {
           800: "#7F7904",
           900: "#645F03",
         },
+      },
+      fontFamily: {
+        'default': ['"Cabinet Grotesk"', '"Segoe UI"', 'sans-serif', ],
       },
     },
   },
