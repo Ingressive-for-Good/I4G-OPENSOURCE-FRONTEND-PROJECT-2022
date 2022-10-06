2232aa17c9c9d1f2e83719a25e2327498b3a5c3a
Boluwatife <omotayodaniel10@gmail.com>
2022-10-06 20:37:07 +0100

    done

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,19 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 
 import loginImage from "../assets/LoginImage.png";
 import webLogo from "../assets/web-logo.png";
 import Arrow from "../assets/Arrow 1.png";
 import PrimaryButton from "./UI/PrimaryButton";
+import SecondaryButton from "./UI/SecondaryButton";
 import Facebook from "../assets/logos_facebook.png";
 import Google from "../assets/icons_google.png";
 import iPadLogo from "../assets/iPad-logo.png";
 import Frame from "../assets/Frame 1.png";
 import MobileLogo from "../assets/mobile-logo.png";
-import HidePassword from "../assets/Hide Password.png";
+import HidePassword from "../assets/Hide.png";
 import ShowPassword from "../assets/Show Passsword.png";
-import SecondaryButton from "./UI/SecondaryButton";
+
+const PASSWORD_REGEX =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 const Login = () => {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [enteredPasswordTouched, setEnteredPasswordTouched] = useState(false);
+  const [passwordType, setPasswordType] = useState("password");
+
+  const togglePasswordType = () => {
+    if (passwordType == "password") {
+      setPasswordType("text");
+      return;
+    }
+    setPasswordType("password");
+  };
+
+  const enteredEmailIsValid = enteredEmail.includes("@");
+  const enteredPasswordIsValid = PASSWORD_REGEX.test(enteredPassword);
+
+  const emailValid = enteredEmailIsValid && enteredEmailTouched;
+  const emailInvalid = !enteredEmailIsValid && enteredEmailTouched;
+  const passwordValid = enteredPasswordIsValid && enteredPasswordTouched;
+  const passwordInvalid = !enteredPasswordIsValid && enteredPasswordTouched;
+
+  let formIsValid = false;
+
+  if (enteredEmailIsValid && enteredPasswordIsValid) {
+    formIsValid = true;
+  }
+
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+  };
+
+  const emailChangeBlur = () => {
+    setEnteredEmailTouched(true);
+  };
+
+  const passwordChangeBlur = () => {
+    setEnteredPasswordTouched(true);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    setEnteredEmailTouched(true);
+    setEnteredPasswordTouched(true);
+
+    if (!enteredEmailIsValid && !enteredPasswordIsValid) {
+      return;
+    }
+
+    setEnteredEmail("");
+    setEnteredPassword("");
+
+    setEnteredEmailTouched(false);
+    setEnteredPasswordTouched(false);
+  };
+
   return (
     <section className="h-screen">
       <div className="w-full flex">
@@ -46,7 +109,7 @@ const Login = () => {
             registration.
           </p>
           <div className="py-4 lg:w-[90%]">
-            <form onSubmit={(e) => e.preventDefault()}>
+            <form onSubmit={formSubmissionHandler}>
               <div className="flex flex-col">
                 <label
                   className="text-neutral-900 font-medium mb-1"
@@ -55,9 +118,15 @@ const Login = () => {
                   Email Address
                 </label>
                 <input
+                  className={`${
+                    emailInvalid && "border-error-500 bg-error-50"
+                  } ${emailValid && "border-success-500 bg-success-50"}`}
                   type="email"
                   id="Email"
                   placeholder="Enter your Email Address"
+                  onChange={emailChangeHandler}
+                  onBlur={emailChangeBlur}
+                  value={enteredEmail}
                 />
               </div>
               <div className="relative flex flex-col">
@@ -67,33 +136,41 @@ const Login = () => {
                 >
                   Password
                 </label>
-                <input type="password" id="Email" placeholder="Password" />
+                <input
+                  className={`${
+                    passwordInvalid && "border-error-500 bg-error-50"
+                  } ${passwordValid && "border-success-500 bg-success-50"}`}
+                  type={passwordType}
+                  id="Email"
+                  placeholder="Password"
+                  onChange={passwordChangeHandler}
+                  onBlur={passwordChangeBlur}
+                  value={enteredPassword}
+                />
                 <img
                   className="w-6 absolute right-4 cursor-pointer translate-y-[170%]"
-                  src={HidePassword}
+                  src={
+                    passwordType === "password" ? HidePassword : ShowPassword
+                  }
                   alt=""
+                  onClick={togglePasswordType}
                 />
               </div>
               <div className="flex justify-between items-center">
-                <div className="flex gap-1 items-center justify-center">
+                <div className="flex gap-2 items-center justify-center">
                   <input
                     className="flex items-center"
                     type="checkbox"
                     id="Remember"
                   />
-                  <label
-                    className="font-medium text-[16px] text-neutral-900"
-                    htmlFor="Remember"
-                  >
-                    Remember me
-                  </label>
+                  <label htmlFor="Remember">Remember me</label>
                 </div>
                 <a className="text-neutral-900 font-bold text-[16px]" href="#">
                   Forgot Password?
                 </a>
               </div>
               <div className="my-6">
-                <PrimaryButton>Log In</PrimaryButton>
+                <PrimaryButton disabled={!formIsValid}>Log In</PrimaryButton>
               </div>
             </form>
             <div className="my-6 flex items-center justify-between">
@@ -103,7 +180,7 @@ const Login = () => {
             </div>
             <div className="flex gap-4 sm:gap-8 items-center md:flex-row flex-col">
               <SecondaryButton>
-                <span className="flex items-center justify-center gap-2 text-center">
+                <span className="flex items-center justify-center gap-2 ">
                   <img src={Google} alt="" />
                   Log in with Google
                 </span>
--- src/Components/UI/PrimaryButton.jsx
+++ src/Components/UI/PrimaryButton.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Button = (props) => {
   return (
-    <button className="bg-primary-500 hover:bg-primary-400 hover:text-neutral-50 text-white py-4  active:bg-primary-700 active:text-neutral-50 w-full rounded-lg">
+    <button
+      disabled={props.disabled}
+      className="bg-primary-500 hover:bg-primary-400 hover:text-neutral-50 text-white py-4  active:bg-primary-700 active:text-neutral-50 disabled:bg-neutral-500 disabled:text-neutral-300 disabled:cursor-not-allowed w-full rounded-lg"
+    >
       {props.children}
     </button>
   );
