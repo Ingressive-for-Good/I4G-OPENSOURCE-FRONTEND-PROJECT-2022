aa141b96a6619a57d3eba5b6ac124dae28be8b4b
ScottDeev <amehelijah99@gmail.com>
2022-10-07 00:20:02 +0100

    added the required fonts

--- src/AddNewProduct/components/Navbar.jsx
+++ src/AddNewProduct/components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
   return (
       <div className='flex flex-row w-full justify-between font-cabinetGrotesk'>
         <div className=''>
-          <h1 className='text-[25px] text-primary-500 text-primary-500 block lg:hidden'>tech<span className='text-secondary-500'>mart</span></h1>
+          <h1 className='text-[25px] font-[700] text-primary-500 text-primary-500 block lg:hidden'>tech<span className='text-secondary-500'>mart</span></h1>
           <h1 className='text-neutral-800 md:text-[20px] lg:mt-0 mt-[20px] text-[16px] font-[500] lg:pb-0 pb-[10px]'>Hey Nora👋🏾</h1>
           <p className='text-neutral-900 md:text-[28px] font-[500] font-[700]'>Add a New Product</p>
           <p className='md:text-[18px] text-[16px] font-[500]'>Ensure that you are filling the <span className='text-secondary-500'>correct</span> and <span className='text-secondary-500 mt-[24px]'>up-to-date</span> information.</p>
--- src/AddNewProduct/components/SideBar.jsx
+++ src/AddNewProduct/components/SideBar.jsx
@@ -9,7 +9,7 @@ import { Link, NavLink } from 'react-router-dom'
 export default function SideBar() {
   return (
     <aside className='font-campton fixed top-0 left-0 h-screen bg-primary-50 w-[230px] pt-[50px] hidden lg:block'>
-      <h1 className='font-cabinetGrotesk text-[32px] text-primary-500 pl-[50px] pb-[38px] text-primary-500'>tech<span className='text-secondary-500'>mart</span></h1>
+      <h1 className='font-cabinetGrotesk text-[32px] font-[700] text-primary-500 pl-[50px] pb-[38px] text-primary-500'>tech<span className='text-secondary-500'>mart</span></h1>
       <div className='flex flex-col justify-between h-[80%]'>
         <ul className='flex flex-col w-full'>
           <h2 className='pl-[50px] pb-[15px] text-primary-500'>MENU</h2>
