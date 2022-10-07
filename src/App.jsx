00f85abad0143c10f9297d02a337f7f7d52dc382
modibboyusuph <ahmadmodibboyusuph3@gmail.com>
2022-10-07 04:38:25 +0100

    PhoneNumberVerificationSuccessUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "../routes";
+import PhoneNumberVerificationSuccessUI from "./molecules/modal/PhoneNumberVerificationSuccessUI";
+
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes />
+      <PhoneNumberVerificationSuccessUI/>
     </BrowserRouter>
   );
 }
