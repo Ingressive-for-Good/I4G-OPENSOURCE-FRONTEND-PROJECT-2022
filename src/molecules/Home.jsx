56809354fba447957b456093095b5180162ba2d3
Treasure Ajefu <treasureajefu@gmail.com>
2022-10-07 11:51:32 +0100

    done with mobile view

--- src/molecules/Home.jsx
+++ src/molecules/Home.jsx
@@ -17,10 +17,12 @@ function App() {
             <span className="text-secondary-500"> one place.</span>
           </h1>
           <p className="text-neutral-700 font-campton my-3">A platform where techies can buy any physical item and also sell or swap items that they are not in use of,  it also create an avenue for them to connect with other creatives around world.</p>
-          <button className="font-cabinetGrotesk bg-primary-500 text-white px-5 py-4 rounded-lg ">Create an account</button>
-          <div className="w-fit mx-auto px-6 py-3 rounded-lg my-2 hover:bg-primary-100 transition-all">
-            <a href="" className="font-cabinetGrotesk text-primary-500 font-semibold">Learn More</a>
-            <img className="mx-auto" src="src/images/landingpage__wavy-arrow.svg" alt="" />
+          <div className="sm:w-fit sm:mx-auto sm:flex sm:justify-center">
+            <button className="font-cabinetGrotesk bg-primary-500 text-white px-5 py-4 rounded-lg sm:h-fit">Create an account</button>
+            <div className="w-fit mx-auto px-6 py-3 rounded-lg my-2 hover:bg-primary-100 transition-all">
+                <a href="" className="font-cabinetGrotesk text-primary-500 font-semibold">Learn More</a>
+                <img className="mx-auto" src="src/images/landingpage__wavy-arrow.svg" alt="" />
+            </div>
           </div>
         </section>
 
@@ -31,7 +33,7 @@ function App() {
             <p className="text-neutral-700">Take control of all your ecommerce transactions</p>
         
             {/* reasons to choose us */}
-            <div className="space-y-4 my-8">
+            <div className="space-y-4 sm:space-y-0 my-8 sm:grid sm:grid-cols-2 sm:gap-5">
                 <div className="bg-primary-50 rounded-lg p-6 max-w-xs mx-auto">
                     <img className="mx-auto" src="src/images/landingpage__best-quality.svg" alt="" />
                     <h4 className="font-cabinetGrotesk font-semibold text-xl my-2">Best Quality</h4>
@@ -47,7 +49,7 @@ function App() {
                     <h4 className="font-cabinetGrotesk font-semibold text-xl my-2">P2P Payment</h4>
                     <p className="text-neutral-700 text-sm">P2P payments are convenient, fast, secured and easy-to-use.</p>
                 </div>
-                <div className="bg-primary-50 rounded-lg p-6 max-w-xs mx-auto">
+                <div className="bg-primary-50 rounded-lg p-6 max-w-xs mx-auto w-full">
                     <img className="mx-auto" src="src/images/landingpage__support.svg" alt="" />
                     <h4 className="font-cabinetGrotesk font-semibold text-xl my-2">24/7 Support</h4>
                     <p className="text-neutral-700 text-sm">Do you have a complaint? <br />Fill this <a href=""> form </a>to get answers ASAP</p>
@@ -105,58 +107,106 @@ function App() {
                 <button className="bg-primary-500 text-white font-medium text-lg px-6 py-5 rounded-lg">List an item now</button>
             </div>
         </section>
-      </main>
 
       {/* products */}
-      <section>
+      <section className="w-5/6 mx-auto">
         <h1 className="text-center text-xl font-semibold font-cabinetGrotesk">Popular Products</h1>
         <img className="mx-auto mb-4" src="src/images/landingpage__wavy-underline.svg" alt="" />
         <p className="text-neutral-700 text-center">View a few products from our techies</p>
       
         {/* product list */}
-        <div>
-            <div>
+        <div className="grid gap-6">
+            <div className=" bg-neutral-50 w-fit mx-auto">
                 <img src="src/images/product-keyboard.png" alt="" />
-                <div>
+                <div className="border-x-2 border-b-2 border-neutral-300 rounded-b-lg flex flex-row justify-between items-center px-6 py-5">
                     <div>
-                        <h5>Workspace Equipment</h5>
-                        <h3>&#8358;100,000.00</h3>         
+                        <h5 className="text-neutral-700 font-medium">Workspace Equipment</h5>
+                        <h3 className="font-cabinetGrotesk font-semibold text-xl">&#8358;25,000.00</h3>         
                     </div>
                     <img src="src/images/cart-icon.svg" alt="" />
                 </div>
             </div>
-            <div>
+            <div className="bg-neutral-50 w-fit mx-auto">
                 <img src="src/images/product-controller.png" alt="" />
-                <div>
+                <div className="border-x-2 border-b-2 border-neutral-300 rounded-b-lg flex flex-row justify-between items-center px-6 py-5">
                     <div>
-                        <h5></h5>
-                        <h3></h3>
+                        <h5 className="text-neutral-700 font-medium">Gaming Pad</h5>
+                        <h3 className="font-cabinetGrotesk font-semibold text-xl">&#8358;100,000.00</h3>
                     </div>
                     <img src="src/images/cart-icon.svg" alt="" />
                 </div>
             </div>
-            <div>
+            <div className="bg-neutral-50 w-fit mx-auto">
                 <img src="src/images/product-controller.png" alt="" />
-                <div>
+                <div className="border-x-2 border-b-2 border-neutral-300 rounded-b-lg flex flex-row justify-between items-center px-6 py-5">
                     <div>
-                        <h5></h5>
-                        <h3></h3>
+                        <h5 className="text-neutral-700 font-medium">Gaming Pad</h5>
+                        <h3 className="font-cabinetGrotesk font-semibold text-xl">&#8358;100,000.00</h3>
                     </div>
                     <img src="src/images/cart-icon.svg" alt="" />
                 </div>
             </div>
-            <div>
+            <div className="bg-neutral-50 w-fit mx-auto">
                 <img src="src/images/product-keyboard.png" alt="" />
-                <div>
+                <div className="border-x-2 border-b-2 border-neutral-300 rounded-b-lg flex flex-row justify-between items-center px-6 py-5">
                     <div>
-                        <h5></h5>
-                        <h3></h3>
+                        <h5 className="text-neutral-700 font-medium">Workspace Equipment</h5>
+                        <h3 className="font-cabinetGrotesk font-semibold text-xl">&#8358;25,000.00</h3>         
                     </div>
                     <img src="src/images/cart-icon.svg" alt="" />
                 </div>
             </div>
         </div>
+        <button className="bg-primary-500 rounded-lg px-6 py-4 font-semibold text-white mx-auto w-fit">See More</button>
       </section>
+
+      {/* newsletter subscription*/}
+        <section className="bg-primary-500 p-6 w-5/6 mx-auto my-16 rounded-lg">
+            <h1 className="text-center text-white text-3xl font-semibold font-cabinetGrotesk">Subscribe for Updates</h1>
+            <img className="mx-auto mb-4" src="src/images/landingpage__wavy-underline.svg" alt="" />
+            <p className="text-white text-center my-5">Subscribe to our newsletter to get the latest updates on our platform</p>
+            <form className="space-y-4 flex flex-col justify-center">
+                <input className="w-full active:border-2 active:border-neutral-300 rounded-lg px-6 py-4" type="text" placeholder="Enter your full name" required/>
+                <input className="w-full active:border-2 active:border-neutral-300 rounded-lg px-6 py-4" type="email" placeholder="Enter your email address" required/>
+                <button className="bg-secondary-500 text-white font-medium text-lg px-6 py-4 rounded-lg mx-auto">Subscribe</button>
+            </form>
+        </section>
+
+        {/* client reviews */}
+        <section className="w-5/6 mx-auto">
+            <h1 className="text-center text-3xl font-cabinetGrotesk font-medium">Client Reviews</h1>
+            <img className="mx-auto" src="src/images/landingpage__wavy-underline.svg" alt="" />
+            <p className="text-center my-3">We serve hundreds of thousands of customers and this is what people are saying about us.</p>
+            
+            {/* review grid */}
+            <div className="space-y-4">
+                <div className="bg-primary-50 rounded-lg p-7">
+                    <img className="mx-auto my-4" src="src/images/landingpage__client-profile-img.svg" alt="" />
+                    <p className="text-neutral-900 text-center leading-relaxed">"techmart is unarguably the best eccomerce platform - a great transforation to Africa"</p>
+                    <h5 className="text-xl font-semibold text-center mt-5">Aisha Issa</h5>
+                    <p className="text-center w-fit mx-auto">techmart Vendor</p>
+                </div>
+                <div className="bg-primary-50 rounded-lg p-7">
+                    <img className="mx-auto my-4" src="src/images/landingpage__client-profile-img.svg" alt="" />
+                    <p className="text-neutral-900 text-center leading-relaxed">"techmart is unarguably the best eccomerce platform - a great transforation to Africa"</p>
+                    <h5 className="text-xl font-semibold text-center mt-5">Aisha Issa</h5>
+                    <p className="text-center w-fit mx-auto">techmart Vendor</p>
+                </div>
+                <div className="bg-primary-50 rounded-lg p-7">
+                    <img className="mx-auto my-4" src="src/images/landingpage__client-profile-img.svg" alt="" />
+                    <p className="text-neutral-900 text-center leading-relaxed">"techmart is unarguably the best eccomerce platform - a great transforation to Africa"</p>
+                    <h5 className="text-xl font-semibold text-center mt-5">Aisha Issa</h5>
+                    <p className="text-center w-fit mx-auto">techmart Vendor</p>
+                </div>
+                <div className="bg-primary-50 rounded-lg p-7">
+                    <img className="mx-auto my-4" src="src/images/landingpage__client-profile-img.svg" alt="" />
+                    <p className="text-neutral-900 text-center leading-relaxed">"techmart is unarguably the best eccomerce platform - a great transforation to Africa"</p>
+                    <h5 className="text-xl font-semibold text-center mt-5">Aisha Issa</h5>
+                    <p className="text-center w-fit mx-auto">techmart Vendor</p>
+                </div>
+            </div>
+        </section>
+      </main>
       {/* <footer></footer> */}
     </div>
   );
