36bf3270d6f029c29c27f2612223826009726de5
dprincecoder <dprincecoder@gmail.com>
2022-10-07 09:53:49 +0100

    fixed issues for email routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,34 @@ import React from "react";
 
 import EmailVerify from "./component/email/EmailVerify";
 
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter >
-    <Routes>
-      <Route path="/email-verification" element={<EmailVerify />} />
-    </Routes>
-</BrowserRouter >
-  
-)}
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <h1 className="text-3xl font-bold text-red-600">Home</h1>
+              <Link to="/support">Help and Support</Link>
+              <br />
+              <Link to="/email-verification">Visit Email EmailVerify Page</Link>
+            </>
+          }
+        />
+        <Route
+          path="/email-verification"
+          element={
+            <>
+              <EmailVerify />
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
