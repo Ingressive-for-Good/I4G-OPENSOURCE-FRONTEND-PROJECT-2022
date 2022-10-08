a7961322da75e84695d84908a3d1f6f7661266cf
dprincecoder <dprincecoder@gmail.com>
2022-10-08 06:30:10 +0100

    touch

--- routes.jsx
+++ routes.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { Route, Routes as Switch, Link } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import Help from "./src/molecules/Help";
-import AddNewProductPage1 from './src/AddNewProduct/pageOne/AddNewProductPage1'
+import AddNewProductPage1 from "./src/AddNewProduct/pageOne/AddNewProductPage1";
 import PhoneNumberVerification from "./src/pages/PhoneNumberVerification";
+import EmailVerify from "./src/component/email/EmailVerify";
 
 // Routes Component Containing Routes For All Base Pages.
 
-function Routes() {
+function Routing() {
   return (
-    <Switch>
+    <Routes>
       <Route
         path="/dashboard"
         element={<h1 className="text-3xl font-bold text-red-600">Dashboard</h1>}
@@ -17,10 +18,7 @@ function Routes() {
         path="/messages"
         element={<h1 className="text-3xl font-bold text-red-600">Messages</h1>}
       />
-      <Route
-        path="/products"
-        element={<AddNewProductPage1/>}
-      />
+      <Route path="/products" element={<AddNewProductPage1 />} />
       <Route
         path="/wishlist"
         element={<h1 className="text-3xl font-bold text-red-600">Wishlist</h1>}
@@ -38,25 +36,30 @@ function Routes() {
         path="/profile"
         element={<h1 className="text-3xl font-bold text-red-600">Profile</h1>}
       />
-       <Route
-        path="/verification"
-        element={<PhoneNumberVerification/>}
-      />
+      <Route path="/verification" element={<PhoneNumberVerification />} />
       <Route
         path="/"
         element={
           <div className="flex flex-col gap-[30px]">
-          {/* Add the link to your page here */}
+            {/* Add the link to your page here */}
             <h1 className="text-3xl font-bold text-red-600">Home</h1>
             <Link to="/support">Help and Support</Link>
             <Link to="/products">Add new product</Link>
             <Link to="/verification">Phone number verification</Link>
-
           </div>
         }
       />
-    </Switch>
+      <Route
+        path="/email-verification"
+        element={
+          <div className="flex flex-col gap-[30px]">
+            <EmailVerify />
+            <Link to="/email-verification">Email verification</Link>
+          </div>
+        }
+      />
+    </Routes>
   );
 }
 
-export default Routes;
+export default Routing;
--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,11 @@
 import React from "react";
-import EmailVerify from "./component/email/EmailVerify";
-import AddNewProductPage1 from './AddNewProduct/pageOne/AddNewProductPage1'
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import Routing from "../routes";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <h1 className="text-3xl font-bold text-red-600">Home</h1>
-              <Link to="/support">Help and Support</Link>
-              <br />
-              <Link to="/email-verification">Visit Email Verification Page</Link>
-              <br />
-              <Link to="/products">Add new product</Link>
-
-            </>
-          }
-        />
-        <Route
-          path="/email-verification"
-          element={
-            <>
-              <EmailVerify />
-            </>
-          }
-        />
-        <Route
-        path="/products"
-        element={<AddNewProductPage1/>}
-        />
-      </Routes>
+      <Routing />
     </BrowserRouter>
   );
 }
