e5d9cf28446369dbab984ef34592ff944e14f5cc
t-bello7 <bello4aus@gmail.com>
2022-10-10 20:13:03 +0100

    add linters

--- utils/services/appSlice.js
+++ utils/services/appSlice.js
@@ -1,14 +1,15 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   price: 0,
 };
 const appSlice = createSlice({
-  name: "app",
+  name: 'app',
   initialState,
   reducers: {
     setPrice: (state, action) => {
       state.price = action.payload.price;
+      return state.price;
     },
   },
 });
