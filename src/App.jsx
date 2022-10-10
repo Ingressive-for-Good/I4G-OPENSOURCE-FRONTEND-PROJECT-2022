b38a74c692fc77ddc4ed6656fe226c865d95cf9e
t-bello7 <bello4aus@gmail.com>
2022-10-10 20:35:49 +0100

    refactor codebase to follow atomic style guide principle

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,16 @@
 import React from "react";
-import EmailVerify from "./component/email/EmailVerify";
-import Home from "./molecules/Home";
+import EmailVerify from "./component/molecules/email/EmailVerify";
+import Home from "./component/molecules/Home";
 import EmptyWishlistViewProducts from "./pages/EmptyWishlistViewProducts";
 import WishlistViewProducts from "./pages/WishlistViewProducts";
-import AddNewProductPage1 from './AddNewProduct/pageOne/AddNewProductPage1'
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import AddNewProductPage1 from './component/molecules/AddNewProduct/pageOne/AddNewProductPage1'
 import Kyc from "./component/kyc-page/Kyc";
-
+import Routes from "./Routes";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Home />
-              <Link to="/support">Help and Support</Link>
-              <br />
-              <Link to="/email-verification">
-                Visit Email Verification Page
-              </Link>
-              <br />
-              <Link to="/products">Add new product</Link>
-              <Link to="/kyc">Kyc Page</Link>
-
-            </>
-          }
-        />
-        <Route
-          path="/email-verification"
-          element={
-            <>
-              <EmailVerify />
-            </>
-          }
-        />
-        <Route
-          path="/kyc"
-          element={
-            <>
-              <Kyc />
-            </>
-          }
-        />
-        
-        <Route
-        path="/products"
-        element={<AddNewProductPage1/>}
-        />
-        <Route 
-            path="/" 
-            element={
-              <div className="p-6">
-              <h1 className="text-3xl font-bold text-red-600">Hello world!</h1>
-              <p>/wishlist (view products in wishlist)</p>
-              <p>/add-to-wishlist (add products to wishlist)</p>
-              </div>
-          } exact></Route>
-          <Route path="/wishlist" element={<WishlistViewProducts />}></Route>
-          <Route  path="/add-to-wishlist" element={<EmptyWishlistViewProducts />}></Route>
-      </Routes>
+        <Routes/>
     </BrowserRouter>
   );
 }
