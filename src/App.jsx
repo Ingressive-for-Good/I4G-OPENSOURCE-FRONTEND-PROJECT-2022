5e28467e9825634e46d536cac27bc5ddbecda9a7
retrong <eguobabarth@gmail.com>
2022-10-14 02:00:06 +0100

    built the empty message page

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,45 @@
 import React from "react";
 import EmptyWishlistViewProducts from "./pages/EmptyWishlistViewProducts";
 import WishlistViewProducts from "./pages/WishlistViewProducts";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import MessageEmpty from "./pages/MessageEmpty";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <>
-        <Router>
-          <Routes>
-          <Route 
-            path="/" 
-            element={
-              <div className="p-6">
-              <h1 className="text-3xl font-bold text-red-600">Hello world!</h1>
-              <p>/wishlist (view products in wishlist)</p>
-              <p>/add-to-wishlist (add products to wishlist)</p>
-              </div>
-          } exact></Route>
-          <Route path="/wishlist" element={<WishlistViewProducts />}></Route>
-          <Route  path="/add-to-wishlist" element={<EmptyWishlistViewProducts />}></Route>
-          </Routes>
-        </Router>
-      </>
+        <BrowserRouter>
+            <Routes>
+                <Route 
+                    path="/"
+                    element={
+                        <>
+                            <Link to="/Messages">Message Page</Link>
+                        </>
+                    }
+                />
+                <Route 
+                    path="/Messages"
+                    element={
+                        <>
+                            <MessageEmpty />
+                        </>
+                    }
+                />
+                <Route 
+                    path="/" 
+                    element={
+                    <div className="p-6">
+                    <h1 className="text-3xl font-bold text-red-600">Hello world!</h1>
+                    <p>/wishlist (view products in wishlist)</p>
+                    <p>/add-to-wishlist (add products to wishlist)</p>
+                    <p>/Messages (Read & Send messages)</p>
+                    </div>
+                } exact></Route>
+                <Route path="/wishlist" element={<WishlistViewProducts />}></Route>
+                <Route  path="/add-to-wishlist" element={<EmptyWishlistViewProducts />}></Route>
+                <Route  path="/Messages" element={<MessageEmpty />}></Route>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/images/index.js
@@ -0,0 +1,17 @@
+import menu from "./menu.svg";
+import dashboard from "./dashboard.svg";
+import messages from "./messages.svg";
+import products from "./products.svg";
+import wishlist from "./wishlist.svg";
+import help from "./help.svg";
+import logout from "./logout.svg";
+
+export {
+    menu,
+    dashboard,
+    messages,
+    products,
+    wishlist,
+    help,
+    logout
+}
