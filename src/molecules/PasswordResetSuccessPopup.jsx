261cb1ab699a446368c1ddf8d16e404ff135dc0d
Osilaja78 <osilajaabdulhameed@gmail.com>
2022-10-14 01:44:40 +0100

    update password reset success popup

--- src/molecules/PasswordResetSuccessPopup.jsx
+++ src/molecules/PasswordResetSuccessPopup.jsx
@@ -4,7 +4,7 @@ import ProceedToLoginButton from "../atoms/Button/ProceedToLoginButton";
 function PasswordResetSuccessPopup() {
     return (
         <div>
-            <div className="text-center">
+            <div className="absolute bg-white top-50 text-center border rounded-lg shadow-lg p-4 m-4">
                 <p className="font-cabinetGrotesk font-bold text-[22px] text-green-600">Password Reset Successful!</p>
                 <img src="" alt="" />
                 <p className="font-campton"><b>Congratulations!</b> Your password has been reset successfully. Try not to forget it anymore.</p>
