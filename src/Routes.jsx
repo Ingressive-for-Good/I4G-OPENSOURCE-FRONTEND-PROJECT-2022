ecfa29df784d1ee04962725688c008c668170c79
EmmanuelObi <obiemmy123@gmail.com>
2022-10-15 16:35:29 +0100

    fixed all conflicts

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes as Switch, Link } from 'react-router-dom';
+import { Route, Routes as Switch } from 'react-router-dom';
 import AddNewProductPage1 from './pages/AddNewProductPage1';
 import PhoneNumberVerification from './pages/PhoneNumberVerification';
 import Login from './pages/Login';
