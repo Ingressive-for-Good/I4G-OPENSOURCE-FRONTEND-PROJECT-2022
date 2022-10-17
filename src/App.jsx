89d4d20eac2ca5d61532981eeb6a97495d78061d
Chibuike-19 <emmanuelobiechina8@gmail.com>
2022-10-17 14:33:27 +0100

    fix routing problem in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Routes from "./Routes";
 
 function App() {
   return (
+    //Any Routes added here won't be merged, please check the routes.jsx file, follow the pattern and add any additional routes.
     <BrowserRouter>
       <Routes>
       </Routes>
