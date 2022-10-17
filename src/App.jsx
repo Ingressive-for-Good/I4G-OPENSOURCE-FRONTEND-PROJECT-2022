0804e7e9e103f1d22491db35f80738bffb4f587f
Scott <amehelijah99@gmail.com>
2022-10-17 21:26:31 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,12 @@ function App() {
     //Any Routes added here won't be merged, please check the routes.jsx file, follow the pattern and add any additional routes.
     <BrowserRouter>
 
- 
-
       <Routes>
+      </Routes>
    
     </BrowserRouter>
   );
 }
 
 export default App;
+
