a6fc5220c2028e4ffd53e417e9baa0ae84d19932
Igwe Acha Akwu <successoracha1995@gmail.com>
2022-10-20 12:48:57 +0100

    added transition animation

--- src/components/atoms/DashboardDialog.jsx
+++ src/components/atoms/DashboardDialog.jsx
@@ -7,7 +7,7 @@ export const DashboardNotificationDialog = ({
 }) => {
   return (
     <div
-      className={`hidden   absolute z-10 mr-3 z-[99] ${
+      className={`hidden  absolute z-10 mr-3 z-[99] ${
         open ? " md:block" : " md:hidden"
       }`}
       aria-labelledby="modal-title"
@@ -15,13 +15,13 @@ export const DashboardNotificationDialog = ({
       aria-modal="true"
     >
       <div
-        className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity  "
+        className="fixed inset-0 bg-gray-500 bg-opacity-75 transition transition-opacity duration-300 "
         onClick={() => {
           setNotificationDialog(!open);
         }}
       ></div>
 
-      <div className="flex items-end  justify-end p-1 text-center sm:items-center sm:p-0 ">
+      <div className="flex transition duration-250 items-end  justify-end p-1 text-center sm:items-center sm:p-0 ">
         <div className="   px-2 pb-2 pt-4 bg-white z-10 notification_clip-path">
           <div className="relative border-gray-200 border-solid border-[2.5px]  transform overflow-hidden rounded-lg bg-white text-left   transition-all   sm:w-full sm:max-w-lg">
             <div className="  px-1 py-2    ">
@@ -31,12 +31,11 @@ export const DashboardNotificationDialog = ({
                     className="text-lg font-medium leading-6 text-gray-900"
                     id="modal-title"
                   >
-                    NOTIFICATION
+                    Notification
                   </h3>
                   <div className="mt-2">
                     <p className="text-sm text-gray-500">
-                      Notification messages will appear here. Implementation is
-                      in progress...
+                      You have no notification right now, come back later!
                     </p>
                   </div>
                 </div>
