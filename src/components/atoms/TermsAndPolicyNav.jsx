5100f501e386c137ec1a38aa1f9215fdf1696282
ScottDeev <amehelijah99@gmail.com>
2022-10-23 12:09:11 +0100

    corrected the style issue by moving the class in the index.css to a new css file

--- src/components/atoms/TermsAndPolicyNav.jsx
+++ src/components/atoms/TermsAndPolicyNav.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
-
 import ArrowLeft from '../../assets/icons/arrow-left.svg'
+
+import "../../../styles/terms.css"
 export default function TermsAndPolicyNav() {
   return (
     <div className="flex lg:flex-row flex-col gap-[50px] lg:bg-primary-50 lg:w-[400px] lg:justify-center pt-[46px]">
@@ -8,8 +9,8 @@ export default function TermsAndPolicyNav() {
         <img src={ArrowLeft} alt="back" />
       </div>
       <div className="flex lg:flex-col gap-[40px]">
-        <NavLink className='sm:text-[24px] text-[18px] text-neutral-900 font-[600]' to='/terms'>Terms of Service</NavLink>
-        <NavLink className='sm:text-[24px] text-[18px] text-neutral-900  font-[600]' to='/policy'>Privacy Policy</NavLink>
+        <NavLink className='navlink sm:text-[24px] text-[18px] text-neutral-900 font-[600]' to='/terms'>Terms of Service</NavLink>
+        <NavLink className='navlink sm:text-[24px] text-[18px] text-neutral-900  font-[600]' to='/policy'>Privacy Policy</NavLink>
       </div>
     </div>
   )
